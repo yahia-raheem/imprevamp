4fe69812fd5ecd5cde2dc8e59ebc325cbe8e3d6a
Yahia <eng.yahiaraheem.93@outlook.com>
2022-07-24 22:14:04 +0200

    feat(blocks): Homepage responsive done

--- templates/blocks/imp-banner/imp-banner-template.php
+++ templates/blocks/imp-banner/imp-banner-template.php
@@ -24,7 +24,7 @@ $bannerButton = mb_get_block_field('banner_button');
             <div class="col-lg-8 d-flex justify-content-start align-items-center">
                 <h3 class="banner-text"><?php echo $bannerText; ?></h3>
             </div>
-            <div class="col-lg-4 d-flex justify-content-end align-items-center">
+            <div class="col-lg-4 d-flex justify-content-start justify-content-lg-end align-items-center">
                 <a href="<?php if ($bannerButton['custom_link']) : echo get_the_permalink($bannerButton['button_post']);
                             else : echo $bannerButton['custom_url'];
                             endif; ?>" class="banner-btn">
